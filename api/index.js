67f07597402a49562e1aff3ad967792c4c6407c4
Greg Landowski <greglando@gmail.com>
2022-05-12 21:52:03 -0700

    built database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-const express, { Router } = require('express')
+const { Router } = require('express')
 const axios = require('axios')
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('stocker.db');
 
 const router = Router()
+
+let stock_pool = null
+
+((async () => {
+    let res = await axios.get('https://query1.finance.yahoo.com/v1/finance/trending/US?count=100')
+})());
 
 router.get('/stock', (req, res) => {
 
